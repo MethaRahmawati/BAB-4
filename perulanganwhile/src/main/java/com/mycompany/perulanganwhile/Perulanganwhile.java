988b382c8e9d286bf3de45df2d9cf94eae06dfb0
MethaRahmawati <145530463+MethaRahmawati@users.noreply.github.com>
2024-09-11 08:01:13 +0700

    Add files via upload

---
+++ perulanganwhile/src/main/java/com/mycompany/perulanganwhile/Perulanganwhile.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.perulanganwhile;
+
+/**
+ *
+ * @author USER
+ */
+public class Perulanganwhile {
+
+    public static void main(String[] args) {
+         int x=0;
+       while (x<10) {           
+           System.out.println("Nilai x ="+x);
+           x++;
+       }
+    }
+}
