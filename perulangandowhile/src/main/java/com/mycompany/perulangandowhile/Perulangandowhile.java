8222d591117edd6530c146a4e3fd671ee132cb4b
MethaRahmawati <145530463+MethaRahmawati@users.noreply.github.com>
2024-09-11 08:00:39 +0700

    Add files via upload

---
+++ perulangandowhile/src/main/java/com/mycompany/perulangandowhile/Perulangandowhile.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.perulangandowhile;
+
+/**
+ *
+ * @author USER
+ */
+public class Perulangandowhile {
+
+    public static void main(String[] args) {
+         int x=0;
+       do{
+           System.out.println("Nilai X ="+x);
+           x++;
+       }while (x<10);
+    }
+}
