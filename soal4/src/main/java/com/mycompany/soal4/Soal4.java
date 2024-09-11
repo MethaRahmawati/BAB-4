77b58006d63cb9f3c38f6fd71899ab3665eab985
MethaRahmawati <145530463+MethaRahmawati@users.noreply.github.com>
2024-09-11 08:01:46 +0700

    Add files via upload

---
+++ soal4/src/main/java/com/mycompany/soal4/Soal4.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.soal4;
+
+/**
+ *
+ * @author USER
+ */
+public class Soal4 {
+
+    public static void main(String[] args) {
+        int jml=0;
+        for(int i=1;i<=10;i++)
+        {
+            jml+=i;
+        }
+        System.out.println("Jumlah Semua Bilangan : "+jml);
+    }
+}
