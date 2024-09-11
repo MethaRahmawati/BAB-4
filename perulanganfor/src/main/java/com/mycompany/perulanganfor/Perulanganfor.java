55a92283486872fc57f805ae77e224280cefa9e3
MethaRahmawati <145530463+MethaRahmawati@users.noreply.github.com>
2024-09-11 08:00:56 +0700

    Add files via upload

---
+++ perulanganfor/src/main/java/com/mycompany/perulanganfor/Perulanganfor.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.perulanganfor;
+
+/**
+ *
+ * @author USER
+ */
+public class Perulanganfor {
+
+    public static void main(String[] args) {
+        int x;
+       for(x=1;x<10;x++){
+           System.out.println("Nilai X "+x);
+    }
+  }
+}
